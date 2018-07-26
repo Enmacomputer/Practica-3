57181316f84eb06d9ed81682a752d5c3b0375488
Enmacomputer <40839110+Enmacomputer@users.noreply.github.com>
2018-07-26 18:03:55 -0400

    Datos

---
+++ practica3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x, y, z;
+			x = 23;
+			y = 24;
+			z = 25;
+		
+
+			Console.WriteLine("Variable x=23");
+			Console.WriteLine("Variable y=24");
+			Console.WriteLine("Variable z=25");
+
+			Console.WriteLine("El resultado de la suma es igual a:" + (x + y + z));
+
+			Console.ReadKey();
+
+
+
+
+		}
+	}
+}
